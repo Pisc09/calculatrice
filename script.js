43b182cfca0e6871960e854e55fbf9e5413dbd1e
Pisc09 <pisco8118@gmail.com>
2025-04-16 13:49:17 +0200

    calcul ne fonctionne

--- script.js
+++ script.js
@@ -7,12 +7,18 @@
 const ecran = document.getElementById("ecrand");
 console.log(ecran);
 
+let valeurNbrs;
+
 const btns = document.querySelectorAll(".calc-btn").forEach((e) => {
   console.log(e.textContent);
   e.addEventListener("click", (e) => {
     console.log(e.target.textContent);
-    ecran.append(e.target.textContent);
-    let valeurNbrs = e.textContent; // ici j'essaie de récupérer les valeurs, mais apparemment ce n'est pas ça, donc je continue de chercher
+    valeurNbrs = e.target.textContent; // ici j'essaie de récupérer les valeurs, mais apparemment ce n'est pas ça, donc je continue de chercher
     console.log(valeurNbrs);
+    const result = eval(valeurNbrs);
+    ecran.append(result);
   });
 });
+
+// let calcul = "2+3+7";
+// ecran.append(eval(calcul));
