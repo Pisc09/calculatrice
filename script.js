9e3cedf58e456d542a12d1e5d4d8063b63b52d8f
Pisc09 <pisco8118@gmail.com>
2025-04-16 12:36:17 +0200

    commentaire récupération valeurs numériques

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const btns = document.querySelectorAll(".calc-btn").forEach((e) => {
   e.addEventListener("click", (e) => {
     console.log(e.target.textContent);
     ecran.append(e.target.textContent);
-    let valeurNbrs = e.textContent;
+    let valeurNbrs = e.textContent; // ici j'essaie de récupérer les valeurs, mais apparemment ce n'est pas ça, donc je continue de chercher
     console.log(valeurNbrs);
   });
 });
